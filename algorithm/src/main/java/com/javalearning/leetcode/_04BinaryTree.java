3c0b4c9561af21492bf3841b41b7d9a960c3f468
hedefu <hedefu999@163.com>
2023-10-15 14:40:49 +0800

    20231015-二叉树层序遍历

--- algorithm/src/main/java/com/javalearning/leetcode/_04BinaryTree.java
+++ algorithm/src/main/java/com/javalearning/leetcode/_04BinaryTree.java
@@ -7,6 +7,7 @@ import com.javalearning.leetcode.utils.TestCases;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class _04BinaryTree {
@@ -214,25 +215,177 @@ static int fibnacci(int N){
 //回溯算法，从二叉树的遍历到多叉树的遍历
 static void traverse2(TreeNode root){
     if (root == null) return;
-    System.out.printf("从节点%s进入节点%s", root, root.left);
+    System.out.printf("从节点%s进入节点%s\n", root, root.left);
     traverse2(root.left);
-    System.out.printf("从节点%s回到节点%s", root.left, root);
+    System.out.printf("从节点%s回到节点%s\n", root.left, root);
+
+    System.out.printf("从节点%s进入节点%s\n", root, root.right);
     traverse2(root.right);
+    System.out.printf("从节点%s回到节点%s\n", root.right, root);
 }
 static void traverseMultiTree(MultiTreeNode root){
     if (root == null) return;
-
+    for (MultiTreeNode branch : root.branches) {
+        System.out.printf("从节点%s进入节点%s\n", root, branch);
+        traverseMultiTree(branch);
+        System.out.printf("从节点%s回到节点%s\n", branch, root);
+    }
 }
-/*
-动态规划、回溯算法就是二叉树算法两种不同的表现形式
-todo 一文秒杀所有岛屿题目 https://labuladong.github.io/algo/di-san-zha-24031/bao-li-sou-96f79/yi-wen-mia-4f482/
-DFS算法和回溯非常类似，差别在于做选择 和 撤销选择，在for循环内外的区别
+/* 此多叉树遍历框架可以延伸出 回溯算法框架
+ * 回溯算法的着眼点是节点之间移动的过程，类比到二叉树上就是 树枝
+static void backtrace(...){
+    for (int i = 0; i <; i++) {
+        //做选择
+        ...
+        //进入下一层决策树
+        backtrace(...);
+        //撤销刚才做的选择
+        ...
+    }
+}
+ * 回溯算法处理全排列问题
+static void backtrace(int[] nums){
+    //回溯算法框架
+    for(int i=0;i<nums.length;i++){
+        //做选择
+        used[i]=true;
+        track.addLast(nums[i]);
+        //进入下一层回溯树
+        backtrace(nums);
+        //取消选择
+        track.removeLast();
+        used[i] = false;
+    }
+}
+ */
 
-讲解过于跳跃，先记下来
+/**
+动态规划、回溯算法就是二叉树算法两种不同的表现形式
+todo BFS算法框架 https://labuladong.github.io/algo/di-ling-zh-bfe1b/bfs-suan-f-463fd/
+todo 一文秒杀所有岛屿题目(DFS算法) https://labuladong.github.io/algo/di-san-zha-24031/bao-li-sou-96f79/yi-wen-mia-4f482/
+todo 回溯算法框架套路详解 https://labuladong.github.io/algo/di-ling-zh-bfe1b/hui-su-sua-c26da/
+todo 回溯算法秒杀排列组合子集的九种题型 https://labuladong.github.io/algo/di-ling-zh-bfe1b/hui-su-sua-56e11/
+todo Dijkstra算法 https://labuladong.github.io/algo/di-yi-zhan-da78c/shou-ba-sh-03a72/dijkstra-s-6d0b2/
+
 动归/DFS/回溯算法都可以看做二叉树问题的扩展，只是它们的关注点不同：
 动态规划算法属于分解问题的思路，它的关注点在整棵「子树」。
 回溯算法属于遍历的思路，它的关注点在节点间的「树枝」。
 DFS 算法属于遍历的思路，它的关注点在单个「节点」。
+*/
+
+/*
+DFS算法和回溯非常类似，差别在于做选择 和 撤销选择，在for循环内外的区别
+正因为DFS关注的是节点，所以做选择和撤销选择的逻辑放在for循环外面，而回溯算法放在里面
+相关伪代码展示
+*/
+static void dfs(MultiTreeNode root){
+    if (root == null) return;
+    System.out.printf("已进入节点%s\n", root);
+    for (MultiTreeNode branch : root.branches) {
+        dfs(branch);
+    }
+    System.out.printf("离开节点%s\n", root);
+}
+//因为回溯算法需要拿到树枝的两个端点，所以需要放到for循环内部
+static void backstrace(MultiTreeNode root){
+    if (root == null) return;
+    for (MultiTreeNode branch : root.branches) {
+        //做选择
+        System.out.printf("过程:节点%s到节点%s\n", root, branch);
+        backstrace(branch);
+        //撤销选择
+        System.out.printf("过程:节点%s到节点%s\n", branch, root);
+    }
+}
+/**LC102 二叉树层序遍历
+ 此题类似LC366 剥离叶子节点
+ */
+static int depth = 0;
+static List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> levelOrderList = new ArrayList<>();
+    levelTraverse(root,levelOrderList);
+    return levelOrderList;
+}
+static void levelTraverse(TreeNode root, List<List<Integer>> levelOrderList){
+    if (root == null) return;
+    List<Integer> currentLevelLeaves = depth >=levelOrderList.size() ? null : levelOrderList.get(depth);
+    if (currentLevelLeaves == null){
+        currentLevelLeaves = new ArrayList<>();
+        levelOrderList.add(currentLevelLeaves);
+    }
+    currentLevelLeaves.add(root.val);
+    depth++;
+    levelTraverse(root.left, levelOrderList);
+    levelTraverse(root.right, levelOrderList);
+    depth--;
+}
+/* 追求递归的更多技巧性，设法避免设置全局变量
+上述二叉树的深度在进入退出节点时维护，这里作为递归入参变量传入
+ */static void levelTraverse(TreeNode root, List<List<Integer>> nodesList, int depth){
+    if (root == null) return;
+    //前序位置
+    if (depth >= nodesList.size()){
+        nodesList.add(new ArrayList<>());
+    }
+    nodesList.get(depth).add(root.val);
+    levelTraverse(root.left, nodesList, depth+1);
+    levelTraverse(root.right, nodesList, depth+1);
+}
+/* 上述使用了递归的思路，本质还是二叉树的前序遍历，或者说DFS/BFS
+这种解法更像是从左到右的"列"序遍历，而不是从上到下的"层"序遍历
+如何真正意义上进行层序遍历？
+* */static List<List<Integer>> levelTraverse(TreeNode root){
+    if (root == null) return new ArrayList<>();
+    List<List<Integer>> nodesList = new ArrayList<>();
+    //不必像递归中那样设置全局变量处理初始化问题
+    LinkedList<TreeNode> nodes = new LinkedList<>();
+    nodes.offer(root);
+    // 第一个while起到纵向遍历的作用
+    while (!nodes.isEmpty()){
+        //如果根据当前 nodes 的size遍历，可以做到仅一个 LinkedList 实例处理遍历
+        //LinkedList 的 offer poll特性可以从左向右遍历，如果使用Stack就可以反过来
+        LinkedList<TreeNode> nextLevelNodes = new LinkedList<>();
+        List<Integer> sameLevelNodes = new ArrayList<>();
+        while (!nodes.isEmpty()){ //第二个while就是横向遍历左右节点
+            TreeNode current = nodes.poll();
+            sameLevelNodes.add(current.val);
+            //交换 left right的offer顺序可以实现自右向左遍历
+            if (current.left != null){
+                nextLevelNodes.offer(current.left);
+            }
+            if (current.right != null){
+                nextLevelNodes.offer(current.right);
+            }
+        }
+        nodesList.add(sameLevelNodes);
+        nodes = nextLevelNodes;
+    }
+    return nodesList;
+}
+/*上述从递归改成了双层循环，其实也可以使用递归进行真正的层序遍历
+递归函数的定义比较特别
+*/
+static void levelTraverse(List<TreeNode> currentLevelNodes){
+    if (currentLevelNodes.isEmpty()){
+        return;
+    }
+    List<TreeNode> nextLevelNodes = new LinkedList<>();
+    for (TreeNode node : currentLevelNodes) {
+        //此处收集同一层的节点val
+        if (node.left != null){
+            nextLevelNodes.add(node.left);
+        }
+        if (node.right != null){
+            nextLevelNodes.add(node.right);
+        }
+    }
+    //除了从左向右的遍历顺序可以颠倒，从上往下的顺序也可以
+    levelTraverse(nextLevelNodes);
+    //后序遍历位置添加当前层的节点val收集结果，可以实现自底向上的倒序遍历
+}
+
+/** LC515 在每个树行中找最大值
+
 */
 
 
@@ -241,9 +394,10 @@ DFS 算法属于遍历的思路，它的关注点在单个「节点」。
         Integer[] i2 = {-10, 9, 20, null, null, 15, 7};
         Integer[] i3 = {-1,-2, 3};
         Integer[] i4 = {1,2,3,4,5,null,null};
-        TreeNode treeNode = TreeNode.plantATree(i4);
-        int result = tellMeDistance(treeNode);
-        System.out.println(resLC366);
+        Integer[] i5 = {3,9,20,5,9,15,7};
+        TreeNode treeNode = TreeNode.plantATree(i5);
+        List<List<Integer>> lists = levelTraverse(treeNode);
+        System.out.println(lists);
     }
 
 //endregion
